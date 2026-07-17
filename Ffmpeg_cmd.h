d2ba894db46e3fa0b7ba2f9b7f77586582c0e45e
tiandic <2323748675@qq.com>
2026-07-17 23:31:53 +0800

    Added support for running ffmpeg across platforms.

--- Ffmpeg_cmd.h
+++ Ffmpeg_cmd.h
@@ -4,6 +4,7 @@
 #include "qdebug.h"
 #include "qdir.h"
 #include "qlist.h"
+#include "qlogging.h"
 #include "qprocess.h"
 #include "qtmetamacros.h"
 #include <QCoreApplication>
@@ -27,15 +28,39 @@ public:
   Q_INVOKABLE void save_ffmpeg_cmd() { config.save_ffmpeg_cmd(); }
 
   Q_INVOKABLE void exec_ffmpeg() {
-    QString exec_path = QDir(QCoreApplication::applicationDirPath())
-                            .filePath("exec_cmd/exec_cmd");
-    QString exec_gui_path = QDir(QCoreApplication::applicationDirPath())
-                                .filePath("exec_cmd_gui/appexec_cmd_gui");
+    QString exec_path = get_exec_path();
+    QString exec_gui_path = get_exec_gui_path();
     QList<QString> argv = {exec_path, config.get_config_path()};
+
+    if (exec_path == "")
+      qCritical() << "Couldn’t find executable file exec_cmd.";
+    if (exec_gui_path == "")
+      qCritical() << "Couldn’t find executable file exec_cmd_gui.";
 
     QProcess::startDetached(exec_gui_path, argv);
   }
 
 private:
   Config_file_ffmpeg_cmd config;
+  QList<QString> exec_paths = {"exec_cmd", "exec_cmd.exe", "exec_cmd/exec_cmd",
+                               "exec_cmd/exec_cmd.exe"};
+  QList<QString> exec_gui_paths = {"exec_cmd_gui", "exec_cmd_gui.exe",
+                                   "exec_cmd/exec_cmd_gui",
+                                   "exec_cmd/exec_cmd_gui.exe"};
+
+  QString find_exists_from_paths(QList<QString> paths) {
+    QString path;
+    for (QString n : paths) {
+      path = QDir(QCoreApplication::applicationDirPath()).filePath(n);
+      QFile file(path);
+      if (file.exists())
+        return path;
+    }
+
+    return "";
+  }
+
+  QString get_exec_path() { return find_exists_from_paths(exec_paths); }
+
+  QString get_exec_gui_path() { return find_exists_from_paths(exec_gui_paths); }
 };
