a4ddbb8b594f293d551eb6c6381e2288efe87453
tiandic <2323748675@qq.com>
2026-07-27 22:37:57 +0800

    Set the font to make sure text styles are consistent across platforms.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
-#include "VideoProvider.h"
+#include <QFontDatabase>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <qfont.h>
+#include <qlist.h>
 
 int main(int argc, char *argv[]) {
   QGuiApplication app(argc, argv);
+
+  QString font_path = ":/fonts/Noto_Sans_Mono_CJK_SC.ttf";
+  int font_id = QFontDatabase::addApplicationFont(font_path);
+  if (font_id == -1) {
+    qWarning() << "font" << font_path << "load fail!";
+  } else {
+    QList<QString> families = QFontDatabase::applicationFontFamilies(font_id);
+    if (!families.isEmpty()) {
+      QFont font(families.at(0));
+      font.setPixelSize(12);
+      app.setFont(font);
+    }
+  }
 
   QQmlApplicationEngine engine;
   QObject::connect(
